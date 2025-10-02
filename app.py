4d07ccaa44a8bab01db07b6fb1a50eabd0a1be8a
DivisorDev <divisordevelopment@gmail.com>
2025-10-02 20:14:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import requests
 
 app = Flask(__name__)
+CROS(app)
 
 API_KEY = "5a5016bed6d4414862e795c51367f019"
 API_URL = "https://api.sunoapi.org/v1/generate"
